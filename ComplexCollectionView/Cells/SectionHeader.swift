935d9828d6483fae51fa5b617b4a89ad4932f870
Aniket <aniketrao1398@gmail.com>
2023-03-12 09:18:58 +0530

    Section Handling
    - Custom header section xib file added.

---
+++ ComplexCollectionView/Cells/SectionHeader.swift
@@ -0,0 +1,20 @@
+//
+//  SectionHeader.swift
+//  ComplexCollectionView
+//
+//  Created by Aniket Rao on 12/03/23.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+       
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    
+}
--- ComplexCollectionView/ViewController.swift
+++ ComplexCollectionView/ViewController.swift
@@ -36,10 +36,13 @@ class ViewController: UIViewController {
         customFlowLayout.sectionInsetReference = .fromContentInset // .fromContentInset is default
         customFlowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         customFlowLayout.headerReferenceSize = CGSize(width: 0, height: 10)
+        customFlowLayout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        
         mainCollectionView.collectionViewLayout = customFlowLayout
         let nib = UINib(nibName: "BiographyCVC", bundle: nil)
         let nib1 = UINib(nibName: "AboutInfoCell", bundle: nil)
         let nib2 = UINib(nibName: "NestedCollectionCVC", bundle: nil)
+        mainCollectionView.register(UINib(nibName : "SectionHeader", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader")
         mainCollectionView.register(nib, forCellWithReuseIdentifier: "BiographyCVC")
         mainCollectionView.register(nib1, forCellWithReuseIdentifier: "AboutInfoCell")
         mainCollectionView.register(nib2, forCellWithReuseIdentifier: "NestedCollectionCVC")
@@ -54,6 +57,27 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
         return 3
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if (indexPath.section == 0 || indexPath.section == 2) {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader", for: indexPath) as! SectionHeader
+            view.lblTitle.text = "Title : \(indexPath.section)"//do other header related calls or settups
+            return view
+            
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if (section == 0 || section == 2) {
+            return CGSize(width: mainCollectionView.frame.width - 20, height: 50)
+            
+        } else {
+            return CGSize.zero
+        }
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if section == 0 {
             return 1
@@ -67,19 +91,16 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.section == 0 {
             guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "BiographyCVC", for: indexPath ) as? BiographyCVC else { return UICollectionViewCell() }
-            
             return cell
         }
         
         if indexPath.section == 1 {
             guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "AboutInfoCell", for: indexPath ) as? AboutInfoCell else { return UICollectionViewCell() }
-            
             return cell
         }
         
         if indexPath.section == 2{
             guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "BiographyCVC", for: indexPath ) as? BiographyCVC  else { return UICollectionViewCell() }
-            
             return cell
             
         }
@@ -88,7 +109,6 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         if indexPath.section == 0 {
             return CGSize(width: view.frame.width - 20, height: 0)
         } else if indexPath.section == 1 {
@@ -96,7 +116,5 @@ extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource
         } else {
             return CGSize(width: view.frame.width - 20 , height: 0)
         }
-
     }
-
 }
